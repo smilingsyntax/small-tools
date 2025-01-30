b8e41089fe9074ca287e1e6c218f67843a1fd93a
codemadan <codemadan@gmail.com>
2025-01-30 11:28:25 +0545

    [add] license added

--- small-tools.php
+++ small-tools.php
@@ -8,8 +8,10 @@
  * Author URI: https://manjul.me
  * Text Domain: small-tools
  * Domain Path: /languages
- * Requires at least: 5.0
- * Requires PHP: 7.2
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 // If this file is called directly, abort.
